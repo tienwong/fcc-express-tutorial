e6bd80e8673597bde123229ca8596582fe1524ee
Tien <tienwong@uwm.edu>
2022-10-01 20:34:08 -0500

    Using express.static middleware to expose assets in public folder so the app can serve a CSS filesheet

--- myApp.js
+++ myApp.js
@@ -1,34 +1,13 @@
 let express = require('express');
 let app = express();
 
+const assetsPath = __dirname + '/public'
+const indexPath = __dirname + '/views/index.html'
+
+app.use('/public', express.static(assetsPath))
+
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(indexPath)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
