80f391b2c05380232287d78e174bde7c7fe14ce8
Tien <tienwong@uwm.edu>
2022-10-02 15:41:19 -0500

    Implemented a simple logger middleware

--- myApp.js
+++ myApp.js
@@ -6,6 +6,16 @@ require('dotenv').config()
 const assetsPath = __dirname + '/public'
 const indexPath = __dirname + '/views/index.html'
 
+const logger = (req, res, next) => {
+    const method = req.method
+    const path = req.path
+    const ip = req.ip
+    console.log(method + ' ' + path + ' - ' + ip)
+    next()
+}
+
+app.use(logger)
+
 app.use('/public', express.static(assetsPath))
 
 app.get('/', (req, res) => {
